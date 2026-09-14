45cc5b7af19173a84a205b3ddac38c9d42d71704
AliyanAmjad986 <aliyanamjad42@gmail.com>
2026-09-14 21:29:25 +0500

    UI Completed

---
+++ Calculator/src/action.js
@@ -0,0 +1,23 @@
+let btnAC = document.getElementById("btn_ac");
+let btnsquare = document.getElementById("btn_square");
+let btnmodule = document.getElementById("btn_module");
+let btndivide = document.getElementById("btn_divide");
+let btn7 = document.getElementById("btn_7");
+let btn8 = document.getElementById("btn_8");
+let btn9 = document.getElementById("btn_9");
+let btnmultiply = document.getElementById("btn_multiply");
+let btn4 = document.getElementById("btn_4");
+let btn5 = document.getElementById("btn_5");
+let btn6 = document.getElementById("btn_6");
+let btnminus = document.getElementById("btn_minus");
+let btn1 = document.getElementById("btn_1");
+let btn2 = document.getElementById("btn_2");
+let btn3 = document.getElementById("btn_3");
+let btnplus = document.getElementById("btn_plus");
+let btn0 = document.getElementById("btn_0");
+let btndot = document.getElementById("btn_dot");
+let btnmix = document.getElementById("btn_mix");
+let btnequal = document.getElementById("btn_equal");
+
+btn7.onclick 
+console.log("geo");
