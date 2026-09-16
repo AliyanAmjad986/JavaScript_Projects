a9822feb4b27b0c1b7fad0aafcb80c038856f48e
AliyanAmjad986 <aliyanamjad42@gmail.com>
2026-09-16 20:47:48 +0500

    plusbtn functionality

--- Calculator/src/action.js
+++ Calculator/src/action.js
@@ -19,42 +19,97 @@ let btndot = document.getElementById("btn_dot");
 let btnmix = document.getElementById("btn_mix");
 let btnequal = document.getElementById("btn_equal");
 let inputbox = document.getElementById("input_bar");
-let addvalues = [];
+let values = [];
 let sum = 0;
+// let operators = [];
+let temp_operator;
+let arraypoint_index = 0;
+let ans = 0;
+
+function operation_performed() {
+  for (let i = arraypoint_index; i < values.length; i++) {
+    if (temp_operator === "+") {
+      ans = ans + values[i];
+    }
+    if (temp_operator === "-") {
+      ans = ans - value[i];
+    }
+  }
+  arraypoint_index++;
+  console.log(ans);
+  inputbox.value = ans;
+}
+
 btn7.addEventListener("click", () => {
   inputbox.value = inputbox.value + "7";
 });
 btnAC.addEventListener("click", () => {
   inputbox.value = "";
-  addvalues.length = 0;
-  sum = 0;
+  values.length = 0;
+  ans = 0;
+  arraypoint_index = 0;
 });
 btn8.addEventListener("click", () => {
   inputbox.value = inputbox.value + "8";
 });
-function addval() {
-  addvalues.push(inputbox.value);
-  for (let i = 0; i < addvalues.length; i++) {
-    addvalues[i] = Number(inputbox.value);
+btn9.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "9";
+});
+btn0.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "0";
+});
+btndot.addEventListener("click", () => {
+  inputbox.value = inputbox.value + ".";
+});
+btnmix.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "8";
+});
+btn3.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "3";
+});
+btn2.addEventListener("click", () => {
+  if (temp_operator === "+" || temp_operator === "-") {
+    inputbox.value = "";
   }
-  inputbox.value = "";
-  for (let i = 0; i < addvalues.length; i++) {
-    sum = sum + addvalues[i];
+  inputbox.value = inputbox.value + "2";
+});
+btn1.addEventListener("click", () => {
+  if (temp_operator === "+" || temp_operator === "-") {
+    inputbox.value = "";
   }
+  inputbox.value = inputbox.value + "1";
+});
+btn6.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "6";
+});
+btn4.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "4";
+});
+btn5.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "5";
+});
+btnmodule.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "8";
+});
 
-  // for(let j = 0; j < addvalues.length; j++ ){
-  //     console.log(addvalues[j]);
-  // }
-  console.log(addvalues.length);
+btndivide.addEventListener("click", () => {});
+btnsquare.addEventListener("click", () => {
+  inputbox.value = inputbox.value + "8";
+});
+
+function display() {
+  for (let i = 0; i < values.length; i++) {
+    console.log(values[i]);
+  }
 }
+// forplus button
 btnplus.addEventListener("click", () => {
-  addval();
+  values.push(Number(inputbox.value));
+  //   operators.push("+");
+  display();
+  temp_operator = "+";
+  operation_performed();
 });
-btnequal.addEventListener("click", () => {
-  addval();
-  inputbox.value = "";
-  inputbox.value = sum;
-  console.log(sum);
-});
-console.dir(inputbox);
-console.log("geo");
+
+//minusbutton
+btnminus.addEventListener("click", () => {});
