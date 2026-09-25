93720189e0ab6fe4685efba991c01c31bb4132c3
AliyanAmjad986 <aliyanamjad42@gmail.com>
2026-09-25 16:39:45 +0500

    update product details

---
+++ Shopping Mall/src/app.js
@@ -0,0 +1,35 @@
+let image_5 = document.getElementById("image_5");
+
+// image_5.style.display = "none";
+// image_5.style.height = "0";
+console.dir(image_5);
+let image_6 = document.getElementById("image_6");
+
+// image_6.style.display = "none";
+// image_6.style.height = "0";
+console.dir(image_6);
+
+let down_button = document.getElementById("slide_arrow_down");
+let up_button = document.getElementById("slide_arrow_up");
+
+let image_1 = document.getElementById("image_1");
+// down_button.addEventListener("click", () => {
+//   image_1.style.display = "none";
+//   image_1.style.height = "0";
+//   image_5.style.display = "block";
+//   image_5.style.height = "70px";
+// });
+
+// up_button.addEventListener("click", () => {
+//   image_1.style.display = "block";
+//   image_1.style.height = "70px";
+//   image_5.style.display = "none";
+//   image_5.style.height = "0";
+// });
+
+let image_4 = document.getElementById("image_4");
+image_4.addEventListener("mouseover", ()=>{
+
+})
+
+
